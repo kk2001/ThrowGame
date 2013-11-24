723b9a62094ee5195ed1f2510ef3ca05d882be87
龙灵修 <qconan@shin-susumumatoMacBook-Air.local>
2013-11-24 21:53:47 +0800

    pushing codes

---
+++ ThrowGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ThrowGameAppController.h
+//  ThrowGame
+//
+//  Created by 龙灵修 on 13-11-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ ThrowGame/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThrowGame
+//
+//  Created by 龙灵修 on 13-11-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
